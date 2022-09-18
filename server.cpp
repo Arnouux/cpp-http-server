9e2900bdba7f91641c019a7425f6b997a2d29941
Arthur Arnx <arthur.arnx@gmail.com>
2022-09-18 23:45:57 +0200

    http repsonse for css fixed

--- server.cpp
+++ server.cpp
@@ -3,22 +3,33 @@
 #include <future>
 #include <iostream>
 #include <vector>
-#include <string.h>
+#include <string>
 #include <algorithm>
 #include <fstream>
-
-std::string header = "HTTP/1.1 200 OK\r\n"
-"Content-Length: 1000\r\n" 
-"Connection: close\r\n"
-"Content-type: text/html\r\n"
-"\r\n";
+#include <time.h>
 
 // todo content-length = file size
 
-char* getHTML() {
-    std::ifstream file("site.html");
+std::vector<char> getHeader(int size, std::string content_type) {
+    char buf[1000];
+    time_t now = time(0);
+    struct tm tm = *gmtime(&now);
+    strftime(buf, sizeof buf, "%a, %d %b %Y %H:%M:%S %Z", &tm);
+
+    std::string s = "HTTP/1.1 200 OK\r\n"
+"Content-Length: " + std::to_string(size-3) + "\r\n" 
+"Connection: close\r\n"
+"Date: " + buf + "\r\n"
+"Content-type: " + content_type + "\r\n"
+"\r\n";
+    std::vector<char> vec(s.begin(), s.end());
+    return vec;
+}
+
+std::vector<char> getDataWithHeader(std::string path, std::string content_type) {
+    std::ifstream file(path, std::ios::binary | std::ios::ate);
     std::vector<char> result;
-    std::vector<char> vec_header(header.begin(), header.end());
+    std::vector<char> vec_header = getHeader(file.tellg(), content_type);
     std::vector<char> vec_html;
 
     if (!file.eof() && !file.fail()) {
@@ -32,8 +43,9 @@ char* getHTML() {
 
     result.insert( result.begin(), vec_header.begin(), vec_header.end() );
     result.insert( result.end(), vec_html.begin(), vec_html.end() );
-    char* bytes = &result[0];
-    return bytes;
+
+
+    return result;
 }
 
 std::vector<std::string> addresses;
@@ -66,7 +78,7 @@ int main() {
 		{
             std::cout << "client accepted:" << inet_ntoa(client_addr.sin_addr) << std::endl;
             //if(std::find(addresses.begin(), addresses.end(), inet_ntoa(client_addr.sin_addr)) != addresses.end()) {
-            if(addresses.size() > 2) {
+            if(addresses.size() > 50) {
                 // todo add timer per address for anti spam
                 std::cout << addresses.size() << std::endl;
             } else {
@@ -75,15 +87,23 @@ int main() {
                 std::vector<char> data(buf, buf + 1024);
                 char *html_needed = NULL;
                 char *css_needed = NULL;
+                char *img_needed = NULL;
                 html_needed = strstr (buf, "text/html");
-                css_needed = strstr (buf, "image/avif");
+                css_needed = strstr (buf, "text/css");
+                img_needed = strstr (buf, "img.png");
+                // todo use GET ./site.css HTTP/1.1 instead
                 if(html_needed) {
-                    char* bytes = getHTML();
+                    std::vector<char> result = getDataWithHeader("site.html", "text/html");
+                    char* bytes = &result[0];
                     send(client, bytes, strlen(bytes)-1, 0);
                     std::cout << "sent" << std::endl;
                 } else if(css_needed) {
-                    // todo CSS getters
-                    char* bytes = getHTML();
+                    std::vector<char> result = getDataWithHeader("site.css", "text/css");
+                    char* bytes = &result[0];
+                    for(int i=0 ; i<10 ; ++i)
+                    {
+                    std::cout << bytes[i];
+                    }
                     send(client, bytes, strlen(bytes)-1, 0);
                     std::cout << "sent css" << std::endl;
                 }
@@ -92,14 +112,15 @@ int main() {
                 //} else {
                     // std::cout << "Request not understood" << std::endl;
                 //}
+                
                 // if Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9
                 // elif Accept: image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8
                 
                 addresses.push_back(inet_ntoa(client_addr.sin_addr));
 
                 // todo append header + current response
-                char* bytes = getHTML();
-                send(client, bytes, strlen(bytes)-1, 0);
+                //char* bytes = getHTML();
+                //send(client, bytes, strlen(bytes)-1, 0);
                 //send(client, response, strlen(response)-1, 0);
             }
         }
@@ -110,6 +131,10 @@ int main() {
 		{
 			std::cout << "Error: " << last_error << std::endl;
 		}
+
+std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        closesocket(client);
+        std::cout << "socket closed" << std::endl;
     }
     
 }
