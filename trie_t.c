d2c80339a23cbdcb76e75403971b1b36ab8c068e
Arthur Arnx <arthur.arnx@gmail.com>
2023-01-20 23:55:45 +0100

    fix datamodel

--- trie_t.c
+++ trie_t.c
@@ -6,24 +6,24 @@
 #include "trie_t.h"
 
 
-int find(trie_node_t root, const char **target) {
-    if(strcmp(target[0], root.word)) {
-        return 0;
-    }
+int find(trie_node_t *root, const char **target) {
+    // if(strcmp(target[0], root->word)) {
+    //     return 0;
+    // }
 
-    trie_node_t current_node = root;
+    trie_node_t *current_node = root;
     int current_token_i = 1;
 
     int found = 1;
     while(found) {
         found = 0;
-        for(int i=0; i<current_node.children_size; ++i) {
-            printf("val: %s\n", current_node.children[i].word);
+        for(int i=0; i<current_node->children_size; ++i) {
+            printf("val: %s\n", current_node->children[i]->word);
             printf("target: %s\n", target[current_token_i]);
-            if(current_node.children[i].word == target[current_token_i]) {
+            if(current_node->children[i]->word == target[current_token_i]) {
                 printf("found\n");
                 current_token_i++;
-                current_node = current_node.children[i];
+                current_node = current_node->children[i];
                 found = 1;
             }
         }
@@ -51,4 +51,73 @@ const char **tokenize_url(const char *url) {
     const char **ctokens;
     ctokens = tokens;
     return ctokens;
+}
+
+void print_from(trie_node_t *root) {
+    printf("%s -> %s -> %s -> %d\n", root->word, root->children[0]->word, root->children[0]->children[0]->word, root->children[0]->children[0]->children_size);
+}
+
+int add_endpoint(trie_node_t *root, const char **add) {
+    trie_node_t *current_node = root;
+    int current_token_i = 0;
+    while(current_token_i < 3) {
+        int found = 0;
+
+        printf("current node (%s) has %d\n", current_node->word, current_node->children_size);
+        for(int i=0; i<current_node->children_size; i++) {
+            if(strcmp(current_node->children[i]->word, add[current_token_i]) == 0) {
+                printf("found\n");
+                found = 1;
+                trie_node_t *node_found = current_node->children[i];
+                current_node = node_found;
+                break;
+            }
+        }
+        if (!found) {
+                trie_node_t **children_list = (trie_node_t **) calloc(10, sizeof(trie_node_t*));
+                trie_node_t *node_to_add = (trie_node_t *) malloc(sizeof(trie_node_t));
+                char *word = (char *) malloc(64);
+                strcpy(word, add[current_token_i]);
+                node_to_add->word=word;
+                node_to_add->is_leaf=false;
+                node_to_add->children_size=0;
+                node_to_add->children=children_list;
+
+                current_node->children[current_node->children_size] = node_to_add;
+                current_node->children_size++;
+
+                current_node = node_to_add;
+        }
+        current_token_i++;
+    }
+    print_from(root);
+}
+
+
+trie_node_t *prepare_trie() {
+    trie_node_t *root = (trie_node_t *) malloc(sizeof(trie_node_t));
+    trie_node_t **children = (trie_node_t **) malloc(sizeof(trie_node_t*) * 10);
+    root->word = "/";
+    root->children = children;
+    root->is_leaf = false;
+    root->children_size = 0;
+    return root;
+}
+
+int main() {
+
+    
+    trie_node_t *root = prepare_trie();
+
+    const char **tokens = tokenize_url("/v1/domain/subdomain");
+    const char **tokens2 = tokenize_url("/v1/domain/subdomain2");
+
+    add_endpoint(root, tokens);
+    add_endpoint(root, tokens2);
+    printf("---------\n");
+    if(find(root, tokens2)) {
+        printf("TRUE\n");
+    } else {
+        printf("FALSE\n");
+    }
 }
--- trie_t.h
+++ trie_t.h
@@ -1,9 +1,10 @@
 typedef struct trie_node_t {
     const char *word;
     bool is_leaf;
-    struct trie_node_t *children;
+    struct trie_node_t **children;
     int children_size;
 } trie_node_t;
 
-int find(trie_node_t root, const char **target);
+int find(trie_node_t *root, const char **target);
 const char **tokenize_url(const char *url);
+int add_endpoint(trie_node_t *root, const char **add);
