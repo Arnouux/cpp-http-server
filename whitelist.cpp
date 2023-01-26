d23ecaff69bc2037e3c9ea2506a10d6e665ddfb6
Arthur Arnx <arthur.arnx@gmail.com>
2023-01-26 23:11:12 +0100

    prepare list vs trie time comparison

--- whitelist.cpp
+++ whitelist.cpp
@@ -1,5 +1,29 @@
 #include "whitelist.hpp"
 #include <fstream>
+#include <vector>
+
+bool Whitelist::isUrlWhitelistedList(std::vector<std::string> whitelist, std::string url) {
+    for(std::string u : whitelist) {
+        if(u == url) {
+            return true;
+        }
+    }
+    return false;
+}
+
+std::vector<std::string> Whitelist::setWhitelistList(std::string whitelist_filename) {
+    std::vector<std::string> vec;
+    std::string line;
+    std::ifstream whitelist_file (whitelist_filename);
+    if (whitelist_file.is_open()) {
+        while ( getline (whitelist_file,line) ) {
+            vec.push_back(line);
+        }
+        whitelist_file.close();
+    }
+    return vec;
+}
+
 
 bool Whitelist::isUrlWhitelisted(trie_node_t *whitelist, std::string url) {
     const url_t *url_tokenized = tokenize_url(url.c_str());
--- whitelist.hpp
+++ whitelist.hpp
@@ -16,6 +16,9 @@ class Whitelist {
     public:
     static bool isUrlWhitelisted(trie_node_t *whitelist, std::string url);
     static trie_node_t *setWhitelist(std::string whitelist_filename);
+
+    static bool isUrlWhitelistedList(std::vector<std::string> whitelist, std::string url);
+    static std::vector<std::string> setWhitelistList(std::string whitelist_filename);
 };
 
 #endif
